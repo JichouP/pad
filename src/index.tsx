a960b2d73a4de66fc4dfdab18fa7126406d9c620
JichouP <akkunn100@gmail.com>
2018-11-19 02:09:19 +0900

    ticker

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,38 @@ import ReactDOM from 'react-dom';
 import { Stage } from 'react-pixi-fiber';
 import Rot from './rot';
 
-class App extends React.Component<{}, {}> {
+interface State {
   width: number;
   height: number;
+}
+
+class App extends React.Component<{}, State> {
+  ticker: PIXI.ticker.Ticker;
   constructor(props: any) {
     super(props);
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
+    this.state = { width: window.innerWidth, height: window.innerHeight };
+    this.ticker = new PIXI.ticker.Ticker();
+    this.ticker.autoStart = true;
+    window.onresize = () => {
+      this.setState({
+        ...this.state,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
   }
   render() {
     return (
       <Stage
-        width={this.width}
-        height={this.height}
+        width={this.state.width}
+        height={this.state.height}
         options={{ backgroundColor: 0x000000 }}
       >
-        <Rot x={this.width / 2} y={this.height / 2} />
+        <Rot
+          x={this.state.width / 2}
+          y={this.state.height / 2}
+          ticker={this.ticker}
+        />
       </Stage>
     );
   }
--- src/rot.tsx
+++ src/rot.tsx
@@ -3,26 +3,29 @@ import * as PIXI from 'pixi.js';
 import { Sprite } from 'react-pixi-fiber';
 import bunny from '../res/bunny.png';
 
-export default class Rot extends Component<
-  { x: number; y: number },
-  { deg: number }
-> {
-  centerAnchor: PIXI.ObservablePoint;
+interface Props {
+  x: number;
+  y: number;
   ticker: PIXI.ticker.Ticker;
+}
+interface State {
+  deg: number;
+}
+
+export default class Rot extends Component<Props, State> {
+  centerAnchor: PIXI.ObservablePoint;
   bunny: PIXI.Texture;
   constructor(props: any) {
     super(props);
     this.state = { deg: 0 };
     this.centerAnchor = new PIXI.ObservablePoint(() => {}, {}, 0.5, 0.5);
-    this.ticker = new PIXI.ticker.Ticker();
-    this.ticker.autoStart = true;
     this.bunny = PIXI.Texture.fromImage(bunny);
   }
   componentDidMount = () => {
-    this.ticker.add(this.animate);
+    this.props.ticker.add(this.animate);
   };
   componentWillUnmount = () => {
-    this.ticker.remove(this.animate);
+    this.props.ticker.remove(this.animate);
   };
   animate = (delta: number) => {
     this.setState({ deg: this.state.deg + delta / 10 });
